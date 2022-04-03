4ab1642867ae49631db2211c51ffe1bb39b87768
Emanuel Targino <emanueltargino1@gmail.com>
2022-04-02 22:53:59 -0300

    change directory path

--- auth-api/src/config/db/initialData.js
+++ auth-api/src/config/db/initialData.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import User from "../../modules/user/models/User.js";
+import User from "../../modules/user/model/User.js";
 
 export async function createInitialData() {
     try {
