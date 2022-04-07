af240491676e67bea1b508cb8f3de8fef1839227
Emanuel Targino <emanueltargino1@gmail.com>
2022-04-06 22:34:58 -0300

    add supplier, category and product class

---
+++ product-api/src/main/java/br/com/curso/productapi/modules/product/model/Product.java
@@ -0,0 +1,35 @@
+package br.com.curso.productapi.modules.product.model;
+
+import br.com.curso.productapi.modules.category.model.Category;
+import br.com.curso.productapi.modules.supplier.model.Supplier;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_SUPPLIER", nullable = false)
+    private Supplier supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_CATEGORY", nullable = false)
+    private Category category;
+
+    @Column(name = "QUANTITY_AVAILABLE", nullable = false)
+    private Integer quantityAvailable;
+}
